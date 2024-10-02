bc9f456c6b5f3d343406b98d6f5d9ce2e2229bb8
SarmadRehman02 <sarmad@devinnovates.com>
2024-10-02 20:22:25 +0500

    RestController is streamlined by defining a new field in the rest-controller class and a load method annotated as @PostConstruct

--- 04-REST-Crud-APIs/src/main/java/com/rest_crud_apis/coding/rest/StudentRestController.java
+++ 04-REST-Crud-APIs/src/main/java/com/rest_crud_apis/coding/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.rest_crud_apis.coding.rest;
 
 import com.rest_crud_apis.coding.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +12,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    List<Student> theStudents;
 
-    //define endpoint "/students"
-
-    @GetMapping("/students")
-    public List<Student> getStudents () {
-
-        List<Student> theStudents = new ArrayList<>();
-
+        //define @PostConstruct to load the student data ..... only once  (when created)
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
         theStudents.add(new Student("Sarmad", "Rehman"));
-        theStudents.add(new Student("wijda", "haider"));
+        theStudents.add(new Student("wijdan", "haider"));
         theStudents.add(new Student("daniyal", "niazi"));
         theStudents.add(new Student("Choi", "Ahmad"));
+    }
+
+
+    //define endpoint "/students"
+    @GetMapping("/students")
+    public List<Student> getStudents () {
         return theStudents;
     }
 }
