b1f446dd8761398b5030df332a1110945107ab11
SarmadRehman02 <sarmad@devinnovates.com>
2024-10-07 15:59:44 +0500

    Delegating the calls from Service layer to DAO

--- 04_1-Rest_Crud/src/main/java/com/RestProject_SpringBoot_4_1/cruddemo/service/EmployeeServiceImpl.java
+++ 04_1-Rest_Crud/src/main/java/com/RestProject_SpringBoot_4_1/cruddemo/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.RestProject_SpringBoot_4_1.cruddemo.service;
 
 import com.RestProject_SpringBoot_4_1.cruddemo.Dao.EmployeeDao;
 import com.RestProject_SpringBoot_4_1.cruddemo.Entity.Employee;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +21,22 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDao.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+       return employeeDao.findById(id);
+
+
+    }
+    @Transactional
+    @Override
+    public Employee save(Employee employee) {
+        return employeeDao.save(employee);
+    }
+    @Transactional
+    @Override
+    public void deleteById(int id) {
+    employeeDao.deleteById(id);
     }
 }
