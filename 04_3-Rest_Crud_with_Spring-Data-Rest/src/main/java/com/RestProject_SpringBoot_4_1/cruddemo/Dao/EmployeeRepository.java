e92771f58161aff7036febc3ef4a90512f19c764
SarmadRehman02 <sarmad@devinnovates.com>
2024-10-08 02:37:23 +0500

    annotated for a changed path for API response

--- 04_3-Rest_Crud_with_Spring-Data-Rest/src/main/java/com/RestProject_SpringBoot_4_1/cruddemo/Dao/EmployeeRepository.java
+++ 04_3-Rest_Crud_with_Spring-Data-Rest/src/main/java/com/RestProject_SpringBoot_4_1/cruddemo/Dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.RestProject_SpringBoot_4_1.cruddemo.Dao;
 
 import com.RestProject_SpringBoot_4_1.cruddemo.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
