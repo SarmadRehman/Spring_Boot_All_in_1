6cc5069038025c16488618c9e557ec2ee7ad6035
SarmadRehman02 <sarmad@devinnovates.com>
2024-10-07 16:16:01 +0500

    Servie layer update for single employee and all

--- 04_1-Rest_Crud/src/main/java/com/RestProject_SpringBoot_4_1/cruddemo/rest/EmployeeRestController.java
+++ 04_1-Rest_Crud/src/main/java/com/RestProject_SpringBoot_4_1/cruddemo/rest/EmployeeRestController.java
@@ -5,6 +5,7 @@ package com.RestProject_SpringBoot_4_1.cruddemo.rest;
 import com.RestProject_SpringBoot_4_1.cruddemo.Entity.Employee;
 import com.RestProject_SpringBoot_4_1.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +27,15 @@ public class EmployeeRestController {
     public List<Employee> findAll() {
         return employeeService.findAll();
     }
+
+    // mapping for a single Emp for GET /employee/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee findById(@PathVariable int employeeId) {
+        Employee theEmp = employeeService.findById(employeeId);
+        if (theEmp == null) {
+            throw new RuntimeException("Employee not found \s" + employeeId);
+        }
+    return theEmp;
+    }
+
 }
