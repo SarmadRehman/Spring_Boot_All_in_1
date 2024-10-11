70eb4c694788f80603e804ffefc29b1f98b3d4f7
SarmadRehman02 <sarmad@devinnovates.com>
2024-10-11 19:06:00 +0500

    Tweaking the response from the model

--- 06-MVC_with_thymeleafdemo/src/main/java/com/springboot/mvc_thymeleaf/thymeleafdemo/controller/HelloWorldController.java
+++ 06-MVC_with_thymeleafdemo/src/main/java/com/springboot/mvc_thymeleaf/thymeleafdemo/controller/HelloWorldController.java
@@ -1,7 +1,11 @@
 package com.springboot.mvc_thymeleaf.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.support.HttpRequestHandlerServlet;
 
 @Controller
 public class HelloWorldController {
@@ -17,4 +21,20 @@ public class HelloWorldController {
     String processForm() {
     return "helloworld";
     }
+    
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest req, Model model) {
+
+       // reading request from HTML
+        String theName = req.getParameter("studentName");
+        // data processing
+        theName = theName.toUpperCase();
+        // message creation
+        String result = "Yo..!!" + theName;
+        // adding to the Model
+        model.addAttribute("message", result);
+        // return the page
+        return "helloworld";
+    }
 }
+
