f0a0eb08c54e8b05e2927c5ddcc89cc034b1cd21
SarmadRehman <sarmadr02@gmail.com>
2024-11-13 20:11:52 +0500

    custom login form for authentication

--- 08_1-demosecurity/src/main/java/com/springsecuritydemi/demosecurity/security/DemoSecurityConfig.java
+++ 08_1-demosecurity/src/main/java/com/springsecuritydemi/demosecurity/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.springsecuritydemi.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -31,5 +33,22 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+    // for custom login form
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage") // for having a GetMapping in controller
+                                .loginProcessingUrl("/authenticateTheUser") // no mapping required provided by the SpringSecurity
+                                .permitAll()
+                );
+
+        return http.build();
     }
 }
